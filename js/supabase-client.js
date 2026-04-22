8ea7e6ecd1d19a844c058d7e8e00e5b52c33d035
Tarun Nayyar <jill.jack.jack4@googlemail.com>
2026-04-22 12:30:15 +0530

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,8 +1,8 @@
 // js/supabase-client.js
 
 // --- IMPORTANT: YAHAN APNI SUPABASE DETAILS DAALEIN ---
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://fxpxsmnakwqczgrhiwkl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4cHhzbW5ha3dxY3pncmhpd2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MjU5MzAsImV4cCI6MjA5MjQwMTkzMH0.-gmIrIiWakuHKOiuDnlW9ZQOJWRntDHUTkzXiU79Hao';
 
 let supabase;
 
